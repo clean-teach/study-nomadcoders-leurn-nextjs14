48498ce65251cf5bbf2b0d65bab51608d05e3185
clean-teach <82921898+clean-teach@users.noreply.github.com>
2024-02-24 00:22:24 +0900

    #3.4 Parallel Requests
    ### 목표
    
    - 영화 상세 페이지에도 로딩 컴포넌트를 적용하여 본다.
    - 두 개의 데이터 소스로부터 Fetch 하여 본다.
        - 많은 데이터 소스로부터 동시에 Fetch 하는 방법에 대해서 알아본다.
        - Data Fetching 방식을 최적화 하는 방법에 대해서 알아본다.
    
    ## 정리
    
    - `Promise.all()` 을 사용하여, 여러 개의 Data Fetch를 병렬적으로 하여 보았다.
        - 기본적으로 Data Fetch 함수를 여러 번 호출하면, 직렬적으로 순서대로 실행 된다.
        - 이는 각각의 fetch 시간이 얼마나 걸릴 지 모르는 비동기 작업에서 비 효율적이다.
    
        → 병렬적으로 fetch 를 실행하면, 동시에 요청을 시작하여 시간을 줄일 수 있다.
    
        → [다음 강의](https://www.notion.so/2024-UPDATE-faa1609b765c4e07a8dc51eabdc9f4d1?pvs=21)에서는 이보다 효과적인 방법인 ReactJS의 **Suspense**를 사용하여 비동기 작업을 최적화 하여본다.
    
    ### 참고
    
    - `Promise.all()`
        - `Promise.all()` 메서드는 순회 가능한 객체에 주어진 모든 프로미스가 이행한 후, 혹은 프로미스가 주어지지 않았을 때 이행하는 Promise를 반환합니다.
        - 일반적으로 다음 코드를 계속 실행하기 전에 서로 연관된 비동기 작업 여러 개가 모두 이행되어야 하는 경우에 사용됩니다.
        - 입력 값으로 들어온 프로미스 중 하나라도 거부 당하면 `Promise.all()`은 즉시 거부합니다.
    
        [Promise.all() - JavaScript | MDN](https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Promise/all)

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,17 +1,26 @@
+import Link from "next/link";
+
 export const metadata = {
     title: "Home",
 };
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-    await new Promise((resolve) => setTimeout(resolve, 5000)); // 로딩 속도를 체감하기 위한 테스트 코드
-    const response = await fetch(URL);
+    const response = await fetch(API_URL);
     const json = await response.json();
     return json;
 }
 
 export default async function RootPage() {
     const movies = await getMovies();
-    return <div>{JSON.stringify(movies)}</div>;
+    return (
+        <div>
+            {movies.map((movie) => (
+                <li key={movie.id}>
+                    <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+                </li>
+            ))}
+        </div>
+    );
 }
