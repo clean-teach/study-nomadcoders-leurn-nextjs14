172ab3aeacb58b22c6c5d44f9445b6f277fbaf73
clean-teach <82921898+clean-teach@users.noreply.github.com>
2024-02-28 20:56:21 +0900

    #4.3 Movie Trailers
    ### 목표
    
    - 영화 상세 페이지 화면을 완성하여 본다.
        - 영화 포스터, 영화 제목, 평점, overview 등을 표시한다.
        - YouTube iframe API 를 사용하여 비디오를 구현하여 본다.
    
    ### 참고
    
    - `Number.prototype.toFixed()`
    
        [Number.prototype.toFixed() - JavaScript | MDN](https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed)
    
        - `toFixed()` 메서드는 숫자를 고정 소수점 표기법(fixed-point notation)으로 표시합니다.
    
        ```tsx
        const numObj = 12345.6789;
        numObj.toFixed(1); // Returns '12345.7': 반올림합니다.
        ```

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -7,9 +7,7 @@ export default async function MovieDetail({
 }: {
     params: { id: string };
 }) {
-
     return <div>
-        <h3>Movie detail page</h3>
         <Suspense fallback={<h1>Loading movie info</h1>}>
             <MovieInfo id={id} />
         </Suspense>
