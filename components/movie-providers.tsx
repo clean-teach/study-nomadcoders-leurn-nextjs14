4705fc9c3409376d336981e4d97b630de06d6db0
clean-teach <82921898+clean-teach@users.noreply.github.com>
2024-03-04 19:26:01 +0900

    #4.4 Dynamic Metadata 코드 챌린지
    - [x]  `/movies/:id/providers`
        - 배열인 “출연진” 데이터와는 다르게, “공급자” 데이터는 객체이다.
            - [x]  먼저 해당 객체를 분석 해보아야 한다.
                - 약어가 가르키는 것이 무엇인지.. 등
    
                → 객체의 최상단 Key 가 가르키는 것은 local, 즉 서비스 지역이다.
    
                - [ ]  ~~현재 브라우저 접속 지역을 기준으로 해당 지역 만을 가져오자.~~
                    - [ ]  ~~해당 기능은 클라이언트 측 기능이다.~~
                    - [x]  어차피 해당 API에서 전체 국가를 서비스 하는것도 아니기 때문에 임의로 “US” 를 선택하여 진행
                        - 1순위 : KR
                        - 2순위 : US
                        - 3순위 : 객체가 존재하는 첫 번째 국가
                - [ ]  ~~객체를 렌더링 하는 방법을 알아보자~~
    
                    → 객체를 렌더링 할 필요가 없다.
    
                - [x]  기타 API 응답 데이터의 경우의 수에 따른 제외 처리

--- components/movie-providers.tsx
+++ components/movie-providers.tsx
@@ -8,19 +8,26 @@ export async function getMovieProviders(id: string) {
 
 export default async function MovieProviders({ id }: { id: string }) {
     const moviesProviders = await getMovieProviders(id);
+    if (Object.keys(moviesProviders).length === 0 || !moviesProviders) {
+        return <div className={styles.container}>공급자 없음</div>
+    }
+    const national = moviesProviders.KR || moviesProviders.US || moviesProviders[Object.keys(moviesProviders)[0]];
+    const providers = national.flatrate || national.buy;
 
     return (
         <div className={styles.container}>
             <dl>
                 <dt>공급자 : </dt>
-                <dd><a href={moviesProviders.US.link}>Themovie DB</a></dd>
+                <dd><a href={national.link}>Themovie DB</a></dd>
             </dl>
             <ul>
-                {moviesProviders.US.buy.map(provider =>
-                    <li key={provider.provider_id}>
-                        <img src={provider.logo_path} alt={provider.provider_name} />
+                {providers.map(provider => {
+                    const imgPath = provider.logo_path.indexOf('http') == 0 ? provider.logo_path : `https://media.themoviedb.org/t/p/original${provider.logo_path}`;
+
+                    return <li key={provider.provider_id}>
+                        <img src={imgPath} alt={provider.provider_name} />
                     </li>
-                )}
+                })}
             </ul>
         </div>
     );
